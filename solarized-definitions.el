5983d53acc946d1c066e48195623987be46eed88
Björn Svensson <bjorn.d.svensson@gmail.com>
2018-02-07 19:37:31 +0100

    Make fringe/line-number column same color as background

--- solarized-definitions.el
+++ solarized-definitions.el
@@ -238,7 +238,7 @@ the \"Gen RGB\" column in solarized-definitions.el to improve them further."
                 (warning (,@fmt-bold ,@fg-red))    ; WarningMsg
                 (success (,@fg-blue))              ; MoreMsg
                 (escape-glyph-face (,@fg-red))
-                (fringe (,@fg-base01 ,@bg-base02))
+                (fringe (,@fg-base01 ,@bg-back))
                 (linum (,@fg-base01 ,@bg-base02))
                 (header-line (,@fg-base0 ,@bg-base02 ,@fmt-revbb)) ; Pmenu
                 (highlight (,@bg-base02))
